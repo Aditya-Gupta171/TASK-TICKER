6c612535148a64463adb62de5ada19b0e8bb6048
Aditya-Gupta171 <adityagupta14april@gmail.com>
2024-01-30 01:27:45 +0530

    update functions

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     const taskInput = document.getElementById('taskInput');
     const addButton = document.getElementById('add-btn');
+    const deleteButton = document.getElementById('delete-btn');
     const taskList = document.getElementById('task-list');
-
     addButton.addEventListener('click', function () {
         const taskText = taskInput.value.trim();
         if (taskText !== '') {
             const listItem = document.createElement('li');
-            listItem.textContent = "Task Scheduled: " + taskText;
+            listItem.innerHTML = '<i class="ri-delete-bin-7-line delete-icon"></i>' + taskText;
             taskList.appendChild(listItem);
             taskInput.value = '';
-            listItem.addEventListener('click', function () {
+            const deleteIcon = listItem.querySelector('.delete-icon');
+            deleteIcon.addEventListener('click', function () {
                 taskList.removeChild(listItem);
             });
         } else {
             alert('Please enter a task!');
         }
+    });
+
+    deleteButton.addEventListener('click', function () {
+        const taskItems = taskList.querySelectorAll('li');
+        taskItems.forEach(function (taskItem) {
+            taskList.removeChild(taskItem);
+        });
     });
 });
